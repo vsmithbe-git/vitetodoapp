57a6a423b82917adad68e6c1c5969a23832704da
vsmithbe-git <vsmithbe.git@gmail.com>
2022-01-27 00:47:38 -0600

    part two of week1

--- app.js
+++ app.js
@@ -25,13 +25,11 @@ const cssClasses = {
   pending:
     "bg-white w-full text-center text-green-500 rounded py-4 border-2 border-green-500 transition transform ease-in-out duration-300 hover:bg-green-500 hover:text-white hover:scale-110 hover:rotate-1 cursor-pointer",
   done:
-    "bg-white w-full text-center text-red-500 rounded py-4 border-2 border-red-500 transition transform ease-in-out duration-300 hover:bg-red-500 hover:text-white hover:scale-110 hover:-rotate-1 cursor-pointer",
-};
+    "bg-white w-full text-center text-red-500 rounded py-4 border-2 border-red-500 transition transform ease-in-out duration-300 hover:bg-red-500 hover:text-white hover:scale-110 hover:-rotate-1 cursor-pointer",};
 
 const updateTodos = () => {
   newList.forEach((list) => {
     const filteredTodos = todos.filter((todo) => todo.status === list.status);
-
     list.element.innerHTML = "";
     filteredTodos.forEach((todo) => {
       const item = document.createElement("li");
