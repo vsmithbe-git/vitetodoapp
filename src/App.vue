5a315b7f47106e82c0fb74c3692bc42f0d20af8f
vsmithbe-git <vsmithbe.git@gmail.com>
2022-02-13 21:31:36 -0600

    Refactoring

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import Title from "./components/Title.vue";
+import List from "./components/List.vue";
+import useTodos from "./composables/useTodos";
+
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-800">
+    <div class="container flex flex-col pt-8 mx-auto space-y-8">
+      <Title/>
+      
+      <div class="flex justify-around">
+        <List />
+        <List isCompleted />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "jit",
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
